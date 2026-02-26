557b4848cda6ca9dc1030bfd0e4ebb1905c6ae5c
Denta Muhajir <denta.muhajir@binus.ac.id>
2026-02-26 17:23:15 +0700

    update: docker

--- src/modules/loan-product/controllers/loan-product.controller.ts
+++ src/modules/loan-product/controllers/loan-product.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from "@nestjs/common";
 import { LoanProductService } from '../services/loan-product.service';
+import { ApiResponse } from "src/shared/response/api-response";
 
 @Controller("api/v1/loan-products")
 export class LoanProductController {
@@ -7,8 +8,14 @@ export class LoanProductController {
     private readonly service: LoanProductService,
   ) {}
 
-  @Get()
+ @Get()
   async listLoanProducts() {
-    return this.service.getLoanCatalog();
+
+    const data = await this.service.getLoanCatalog();
+
+    return ApiResponse.success(
+      "Loan products fetched successfullyy",
+      data,
+    );
   }
 }
